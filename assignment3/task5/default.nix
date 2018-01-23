614c453aacafb62be0972a65d3e0aff1a894fa56
Thomashrb <thomas@beat.no>
2018-01-23 22:50:27 +0100

    add deps

--- assignment3/task5/default.nix
+++ assignment3/task5/default.nix
@@ -1,10 +1,13 @@
-{ mkDerivation, base, bytestring, stdenv, turtle }:
+{ mkDerivation, base, bytestring, directory, stdenv, text, turtle
+}:
 mkDerivation {
   pname = "task5";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base bytestring turtle ];
+  executableHaskellDepends = [
+    base bytestring directory text turtle
+  ];
   license = stdenv.lib.licenses.mit;
 }
--- assignment3/task5/shell.nix
+++ assignment3/task5/shell.nix
@@ -4,14 +4,18 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, base, bytestring, stdenv, turtle }:
+  f = { mkDerivation, base, bytestring, directory, stdenv, text
+      , turtle
+      }:
       mkDerivation {
         pname = "task5";
         version = "0.1.0.0";
         src = ./.;
         isLibrary = false;
         isExecutable = true;
-        executableHaskellDepends = [ base bytestring turtle ];
+        executableHaskellDepends = [
+          base bytestring directory text turtle
+        ];
         license = stdenv.lib.licenses.mit;
       };
 
