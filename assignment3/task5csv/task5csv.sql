2dca84c41c272a32b9bca0fc4cd9ac29b892090c
skolethomas <117408@stud.hvl.no>
2018-01-29 14:26:37 +0100

    add denormalize

--- assignment3/task5csv/task5csv.sql
+++
@@ -1,6 +0,0 @@
-LOAD DATA LOCAL INFILE '/home/bbsl/development/dat151/assignment3/task5csv/data.csv'
-INTO TABLE allrow
-FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\n'
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
- (arrID, arrName, arrTime, arrSpaces, pID, pFname, pLname);
