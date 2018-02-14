e2bd78f4b0de8ac51cbfb817a775119e0b96cc69
Thomashrb <thomas@beat.no>
2018-02-14 14:25:47 +0100

    start writing assignment 7 on drive

---
+++ assignment7/dbbackup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+echo "FLUSH TABLES WITH READ LOCK;" | mysql tolldb
+
+#save file suffix and pos which we use later
+suffix=$(echo "SHOW MASTER STATUS" | mysql -N tolldb | awk '{print $1}')
+position=$(echo "SHOW MASTER STATUS" | mysql -N tolldb | awk '{print $2}')
+
+#dump the db naming it according to the log position and numbering
+mysqldump -B -F -u root mysql > tolldb_$suffix_$position.dump
+
+echo "UNLOCK TABLES;" | mysql tolldb
