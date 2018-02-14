12512b2d34a3e31c39577db6073a9943a4fd0e83
Thomashrb <thomas@beat.no>
2018-02-14 14:38:31 +0100

    add cut to suffix so cut out "bin." from variable name

--- assignment7/dbbackup.sh
+++ assignment7/dbbackup.sh
@@ -2,7 +2,7 @@
 echo "FLUSH TABLES WITH READ LOCK;" | mysql tolldb
 
 #save file suffix and pos which we use later
-suffix=$(echo "SHOW MASTER STATUS" | mysql -N tolldb | awk '{print $1}')
+suffix=$(echo "SHOW MASTER STATUS" | mysql -N tolldb | awk '{print $1}') | cut -c5-
 position=$(echo "SHOW MASTER STATUS" | mysql -N tolldb | awk '{print $2}')
 
 #dump the db naming it according to the log position and numbering
