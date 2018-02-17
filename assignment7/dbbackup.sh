2affcd69fad3fae7d72b50640337b78f6aff57b7
Thomashrb <thomas@beat.no>
2018-02-17 18:22:42 +0100

    hotfix minor comforts

--- assignment7/dbbackup.sh
+++ assignment7/dbbackup.sh
@@ -2,10 +2,13 @@
 echo "FLUSH TABLES WITH READ LOCK;" | mysql tolldb
 
 #save file suffix and pos which we use later
-suffix=$(echo "SHOW MASTER STATUS" | mysql -N tolldb | awk '{print $1}') | cut -c5-
+suffix=$(echo "SHOW MASTER STATUS" | mysql -N tolldb | awk '{print $1}' | cut -c4-)
 position=$(echo "SHOW MASTER STATUS" | mysql -N tolldb | awk '{print $2}')
 
 #dump the db naming it according to the log position and numbering
-mysqldump -B -F -u root tolldb > tolldb_$suffix_$position.dump
+mysqldump -F -u root tolldb > tolldb$position$suffix.dump
+
+unset suffix
+unset position
 
 echo "UNLOCK TABLES;" | mysql tolldb
